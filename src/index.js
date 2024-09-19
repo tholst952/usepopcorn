6afae8070895923ed92cdf1837247b7310d16349
trena <20079502+tholst952@users.noreply.github.com>
2024-09-19 13:09:15 -0700

    added StarRating as a standalone component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-// import StarRating from "./StarRating";
-import HeartRating from "./HeartRating.js";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <StarRating maxRating={10} /> */}
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
-    <HeartRating maxHearts={30} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={8} color="crimson" />
+    <StarRating size={24} maxRating={12} color="tomato" className="test" />
+    <StarRating maxRating={12} color="teal" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
